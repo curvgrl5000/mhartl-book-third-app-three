29ad05bf5b8f36eeec55b1a6cbf2a7a98dacd003
anna mendoza <anna@curvecommunications.biz>
2014-01-04 11:28:53 -0800

    updated test again so that everything would pass

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,12 +12,12 @@ describe "Static pages" do
      it "should have the base title" do
        visit '/static_pages/home'
        page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App")
+                        :text => "RonR Sample App")
     end
     
-     it "should have the custom page title" do
+     it "should have the title 'Home" do
        visit '/static_pages/home'
-       page.should_not have_selector('title', :text => '| Home')
+       page.should have_selector('title', :text => '| Home')
     end
   end
   
@@ -31,7 +31,7 @@ describe "Static pages" do
        it "should have the title 'Help'" do
          visit '/static_pages/help'
          page.should have_selector('title',
-                          :text => "Ruby on Rails Tutorial Sample App | Help")
+                          :text => "RonR Sample App | Help")
       end
     end
   
@@ -44,7 +44,7 @@ describe "Static pages" do
        it "should have the title 'About Us'" do
          visit '/static_pages/about'
          page.should have_selector('title',
-                          :text => "Ruby on Rails Tutorial Sample App | About Us")
+                          :text => "RonR Sample App | About Us")
     end
   end
   
@@ -57,7 +57,7 @@ describe "Static pages" do
        it "should have the title 'Contact'" do
          visit '/static_pages/contact'
          page.should have_selector('title',
-                          :text => "Ruby on Rails Tutorial Sample App | Contact")
+                          :text => "RonR Sample App | Contact")
     end
   end
 end
