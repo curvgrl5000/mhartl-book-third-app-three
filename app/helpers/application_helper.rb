7aeacfb8db3ac81028d82748752710b8b9fd4a73
anna mendoza <anna@curvecommunications.biz>
2014-01-05 16:01:42 -0800

    finessing the application helper one more time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   
   # Returns the full title on a per-page basis.            # Documentation comment
-  def full_title(page_title)                               # Method definition
-    base_title = "RonR Sample App"       # Variable assignment
+  def full_title(page_title)                               # Method definition with 1 argument
+    base_title = "RonR Sample App"                         # Variable assignment
       if page_title.empty?                                 # Boolean test
-        base_title                                         # Implicit return
+       return base_title                                    # Implicit return of the var 'base_title'  
       else
         "#{base_title} | #{page_title}"                    # String interpolation
       end
