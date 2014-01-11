3bd8bc343dabff3d6ed69dcb51980fea693e7f80
anna mendoza <anna@curvecommunications.biz>
2014-01-11 01:37:31 -0800

    updated the utilities and modified the static_pages

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,12 @@
-def full_title(page_title)
-  base_title = "RonR Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
+# The following code is uncommented and replaced with 'include ApplicatonHelper'  referencing the file # under the folder 'helpers'. Flesh out the reasoning for this with some documentation.
+
+# def full_title(page_title)
+#   base_title = "RonR Sample App"
+#   if page_title.empty?
+#     base_title
+#   else
+#     "#{base_title} | #{page_title}"
+#   end
+# end            
