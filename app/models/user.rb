54e06b8f4f0afc268ee1d6dc4998d8b132cd2bad
anna mendoza <anna@curvecommunications.biz>
2014-01-18 15:36:59 -0800

    modified the sessions_helper with the sign_in function which spec'd how the cookies object  works with the remember_token attribute 'permanently'.... and updated the models/user.rb with a private method 'create_remember_token' which returns the expression back to self.

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base                                      # User clas
   
   # before_save { |user| user.email = email.downcase }
   before_save { email.downcase! }                                     # an alternate implementation of the 'before_save' callback 
-  
+  before_save :create_remember_token
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -30,5 +30,9 @@ class User < ActiveRecord::Base                                      # User clas
             uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-
+  
+  private  # HELLO THIS ONE IS PROVIATE!!!!!!!!!!!!!!!!
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
