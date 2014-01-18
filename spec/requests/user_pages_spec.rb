31a04a59bf4d6d3d78827c357647fc0ed3d12fcb
anna mendoza <anna@curvecommunications.biz>
2014-01-18 00:36:13 -0800

    updated spec and got it to pass

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,8 +5,9 @@ describe "User pages" do
   subject { page }
 
   describe "profile page" do
-    let(:user)  { FactoryGirl.create(:user) }
     before { visit user_path(user) }
+    let(:user)  { FactoryGirl.create(:user) }
+
     
     it { should have_content(user.name) }
     it { should have_selector('h1', text: user.name) }
@@ -17,6 +18,7 @@ describe "User pages" do
   describe "signup" do
     before {  visit signup_path  }
     let(:submit) { "Create my account" }
+
     
     describe "with invalid information" do
       before do
@@ -29,35 +31,34 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+    
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should_not have_selector('h1', text: 'Sign Up') }
+        it { should_not have_content('error') }
+      end
     end
+    
+    describe "with valid information" do
+      let(:user)  { FactoryGirl.create(:user) }
+      it "should create a user" do
+        expect { click_button submit }.not_to change(User, :count).by(1)
+      end
+      
+           
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('h1', text: '') }
+        it { should have_content('error') }
+      end
+    end  
   end
 end
 
 
-
-# require 'spec_helper'
-# 
-# describe "User pages" do
-# 
-#   subject { page }
-# 
-#   describe "profile page" do
-#     let(:user)  { FactoryGirl.create(:user) }
-#     before { visit user_path(user) }
-#     
-#     it { should have_content(user.name) }
-#     it { should have_selector('h1', text: user.name) }
-#     it { should have_selector('title', text: user.name) }
-#   end
-#   
-#   
-#   describe "signup page" do
-#     before {  visit signup_path  }
-#   
-#     it { should have_selector('h1', text: 'Sign up') }
-#     it { should have_selector('title', text: full_title('Sign Up'))  }
-#   end
-# end  
 
 
 
